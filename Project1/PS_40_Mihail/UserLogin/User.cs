6221dbf796704f81cb48feffb231271a5f9a989e
Mihail Damyanov <m.damyanov.d@gmail.com>
2018-02-25 21:26:31 +0200

    Add files via upload

---
+++ Project1/PS_40_Mihail/UserLogin/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLogin
+{
+    class User
+    {
+        public string username;
+        public string password;
+        public string facNumber;
+        public Int32 roleId;
+    }
+}
